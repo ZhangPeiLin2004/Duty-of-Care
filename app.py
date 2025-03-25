890d375f2c4fe94178f5490840be1e64f0ee36ec
ZhangPeiLin2004 <peilin.zhang8888@gmail.com>
2025-03-25 15:30:14 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+@app.route("/causes")
+def causes():
+    return render_template("causes.html")
+
+@app.route("/solutions")
+def solutions():
+    return render_template("solutions.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
